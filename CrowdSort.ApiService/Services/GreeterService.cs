6f33bc3edd97bc0c4809163a09d7b839a771542d
rcardona8 <rcardona8@live.com>
2025-04-30 10:04:36 -0400

    Summary: Enhance gRPC support and refactor application
    * Updated `CrowdSort.ApiService.csproj` and `CrowdSort.Web.csproj` to include new properties and package references for gRPC support.
    * Modified `Program.cs` to integrate gRPC services and the new `CrowdSortApiClient`, along with updates to Redis output caching.
    * Adjusted `launchSettings.json` to streamline application URLs and remove unnecessary profiles.
    * Improved layout and navigation by updating `App.razor`, `MainLayout.razor`, and `NavMenu.razor`, removing unused elements.
    * Removed `Counter.razor` and `Weather.razor` components to shift focus away from these features.
    * Enhanced `Error.razor` and `Home.razor` components with output caching and improved error handling.
    * Added `greet.proto` to define a new gRPC service, along with `GreeterService.cs` and `CrowdSortApiClient.cs` for implementation.
    * Introduced `Hello.razor` component for user interaction with the new gRPC service, allowing personalized greetings.

---
+++ CrowdSort.ApiService/Services/GreeterService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace CrowdSort.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> Hello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Hello, {request.Name}!",
+            });
+        }
+    }
+}
